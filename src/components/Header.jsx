e4837921d46ed7b3f76a5173a9fceec0d908adb5
Javier Figueroa <109968115+javiernfigueroa@users.noreply.github.com>
2023-05-05 15:51:38 -0400

    header component update

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+export default function Header() {
+    return (
+        <header>
+            <img src="./src/assets/images/troll-face.png" alt="" />
+            <h1>Meme Generator</h1>
+            <h3>React Course - Proyect 3</h3>
+        </header>
+    )
+}
